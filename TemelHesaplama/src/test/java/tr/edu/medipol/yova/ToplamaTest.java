663f0c8bdf047f5459465dec8bc855eff9c6b985
Ozkan Sari <ozkansari@gmail.com>
2023-05-16 20:42:32 +0300

    Update ToplamaTest.java
    testToplaIkiPozitifSayi eklendi

--- TemelHesaplama/src/test/java/tr/edu/medipol/yova/ToplamaTest.java
+++ TemelHesaplama/src/test/java/tr/edu/medipol/yova/ToplamaTest.java
@@ -20,6 +20,19 @@ public class ToplamaTest {
 	}
 	
 	@Test
+	public void testToplaIkiPozitifSayi() {
+		// GIVEN - ONKOSULLAR
+		int sayi1 = 99;
+		int sayi2 = 1;
+		
+		// WHEN - ASIL TEST
+		int gercekSonuc = Toplama.islemYap(sayi1, sayi2);
+		
+		// THEN - KONTROL / VALIDASYON
+		assertEquals(100, gercekSonuc);
+	}
+	
+	@Test
 	public void testToplaIkiNegatifSayi() {
 		// GIVEN - ONKOSULLAR
 		int sayi1 = -99;
